66ce7d787a1b28ee999500e77ca0921b5e053bd7
Lukas Frystak <lukas.frystak@sonarsource.com>
2024-10-25 15:15:08 +0200

    Tweak rename script
    - Log if a name appears more than twice.
    - Throw an error and stop if a file name can't be renamed.

--- RenameCpyFiles/Rename.ps1
+++ RenameCpyFiles/Rename.ps1
@@ -3,7 +3,19 @@ $cpyFilesInfo = Get-ChildItem -Path EOCF -File -Recurse -Filter '*.cpy'
 Write-Output "$($cpyFilesInfo.Count) files with extension ""*.cpy"" found"
 
 $duplicatedFileNamesGroupInfoObjects = $cpyFilesInfo | Group-Object -Property Name | Where-Object { $_.Count -gt 1 }
-Write-Output "$($duplicatedFileNamesGroupInfoObjects.Count) duplicated files found"
+Write-Output "$($duplicatedFileNamesGroupInfoObjects.Count) duplicated file names found"
+
+# Log names that appear more than 2 times
+$a = $duplicatedFileNamesGroupInfoObjects | Where-Object { $_.Count -gt 2 }
+if ($a.Count -gt 0) {
+    Write-Output ""
+    Write-Output "Some file names appear more than twice..."
+    Write-Output "  File name: $($a.Name)"
+    foreach ($name in $a.Group) {
+        Write-Output "    $($name)"
+    }
+    Write-Output ""
+}
 
 if (($null -ne $duplicatedFileNamesGroupInfoObjects) -and ($duplicatedFileNamesGroupInfoObjects.Count -gt 0)) {
     # Select the groups of duplicated files so that we can rename the duplicates
@@ -14,7 +26,17 @@ if (($null -ne $duplicatedFileNamesGroupInfoObjects) -and ($duplicatedFileNamesG
     $filesToRename = $duplicatedFileNamesGroups | Where-Object { $_.Directory.Name -ne "COPYPROC" }
     Write-Output "$($filesToRename.Count) files will be renamed"
     Write-Output "Changing file extensions..."
-    $filesToRename | Rename-Item -NewName { $_.Name -replace '.cpy', '.cpyx' } -Verbose
+
+    foreach ($file in $filesToRename) {
+        try {
+            $file | Rename-Item -NewName { $_.Name -replace '.cpy', '.cpyx' } -Verbose -ErrorAction Stop
+        }
+        catch {
+            Write-Output "Failed to rename file $file"
+            $_.Exception
+            Exit 1
+        }
+    }
     Write-Output "File extensions changed"
 }
 else {
