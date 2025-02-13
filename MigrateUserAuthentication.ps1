cc82b81332f3913a164027d67861acd37fe77c46
Lukas Frystak <lukas.frystak@sonarsource.com>
2025-02-13 11:12:27 +0100

    Fix user migration script.
    The previous version only worked in
    Powershell 7.X but not in Powershell 5
    that ships with Windows. It was needed
    to change curl to Invoke-WebRequest.

--- MigrateUserAuthentication.ps1
+++ MigrateUserAuthentication.ps1
@@ -1,13 +1,25 @@
-# SonarQube connection parameters
-$TOKEN = "";
-$SONARQUBE_URL = ""
+# Required setup
+$TOKEN = "squ_3f2bb67dcd83212242375673360bd0e935d40e48";
+$SONARQUBE_URL = "http://localhost:20251"
 
 # Email of the user to migrate to SAML and the intended external identity.
-$USER_EMAIL = ""
-$USER_EXTERNAL_ID = $USER_EMAIL.ToLower()
+$USER_EMAIL = "ahoj@test.com"
+$USER_EXTERNAL_ID = "ahoj-tets"
 
-# Get all users
-$users = $(curl -s --header "Authorization: Bearer $TOKEN" --url "$SONARQUBE_URL/api/v2/users-management/users" | ConvertFrom-Json).users
+########################################################################
+
+# Authorization token must be passed in a header.
+$httpRequestHeaders = @{
+    "Authorization" = "Bearer $TOKEN"
+}
+
+# Get users: search by the user's email.
+# Note: the request doesn't handle paging, but it shouldn't
+#       be needed since we are searching by their email.
+$users = $(Invoke-WebRequest -Headers $httpRequestHeaders `
+        -Method GET `
+        -Uri "$SONARQUBE_URL/api/v2/users-management/users?q=$USER_EMAIL" `
+    | ConvertFrom-Json).users
 
 # Find the required user
 $requiredUser = $users | Where-Object { $_.email -eq $USER_EMAIL }
@@ -16,21 +28,19 @@ if ($null -eq $requiredUser) {
     Exit 1
 }
 
-# Extract the users ID
-$SQ_USER_ID = $requiredUser.id
-
 # Create the body for the request with the updated information.
 $body = "{""externalProvider"": ""saml"", ""externalLogin"": ""$USER_EXTERNAL_ID""}"
 try {
     $body | ConvertFrom-Json > $null
 }
-catch{
+catch {
     Write-Error "Invalid JSON body"
     Exit 1
 }
 
 # Execute the user PATCH request
-curl `
-    -s --header "Authorization: Bearer $TOKEN" `
-     -X PATCH -H "Content-Type: application/merge-patch+json" -d $body `
-    --url "$SONARQUBE_URL/api/v2/users-management/users/$SQ_USER_ID" | ConvertFrom-Json
+Invoke-WebRequest -Headers $httpRequestHeaders `
+    -Method PATCH `
+    -ContentType "application/merge-patch+json" -Body $body `
+    -Uri "$SONARQUBE_URL/api/v2/users-management/users/$($requiredUser.id)" `
+| ConvertFrom-Json
